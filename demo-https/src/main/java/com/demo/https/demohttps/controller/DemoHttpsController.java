4c4db2ac3d8ec6c7f14dff63c9dddb6d605a2cb4
lixueyang <lixueyang@zccninfo.com>
2019-03-21 11:01:16 +0800

    添加测试程序

--- demo-https/src/main/java/com/demo/https/demohttps/controller/DemoHttpsController.java
+++ demo-https/src/main/java/com/demo/https/demohttps/controller/DemoHttpsController.java
@@ -2,6 +2,11 @@ package com.demo.https.demohttps.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DemoHttpsController {
@@ -9,5 +14,14 @@ public class DemoHttpsController {
     @GetMapping("/index")
     public String onTestHttps() {
         return "/index";
+    }
+
+    @PostMapping("/users")
+    @ResponseBody
+    public Object onUserList() {
+        Map user = new HashMap<>();
+        user.put("name", "sunroom");
+        user.put("nickname", "sunroom");
+        return user;
     }
 }
