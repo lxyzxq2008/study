aae0e6c75b2269950677262e9901ea3d938de8ff
lixueyang <lixueyang@zccninfo.com>
2019-03-21 10:43:04 +0800

    添加测试程序

---
+++ demo-https/src/main/java/com/demo/https/demohttps/DemoHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.https.demohttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHttpsApplication.class, args);
+	}
+
+}
